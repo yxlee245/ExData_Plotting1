11ee068bcfb137151e67eaad7816a638d461a12e
yxlee245 <41932856+yxlee245@users.noreply.github.com>
2020-02-19 22:17:15 +0800

    Refactored code to process date and time

--- plot2.R
+++ plot2.R
@@ -1,14 +1,5 @@
-# Load libraries
-library(lubridate)
-
 # Read in data
 source("read_data.R")
-
-# Concatenate Date and Time
-df_subset$DateTime <- paste(df_subset[, "Date"], df_subset[, "Time"],
-                            sep = " ")
-# Parse DateTime as POSIX objects
-df_subset$DateTime <- dmy_hms(df_subset$DateTime, tz = "CET")
 
 # Plot graph
 plot(df_subset$DateTime, df_subset$Global_active_power,
--- read_data.R
+++ read_data.R
@@ -1,5 +1,16 @@
+# Load libraries
+library(lubridate)
+
 # Read in data and get subset of data for 2007-02-01 and 2007-02-02
 df <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";",
                  na.strings = "?", stringsAsFactors = FALSE)
 df_subset <- subset(df, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Concatenate Date and Time
+df_subset$DateTime <- paste(df_subset[, "Date"], df_subset[, "Time"],
+                            sep = " ")
+# Parse DateTime as POSIX objects
+df_subset$DateTime <- dmy_hms(df_subset$DateTime, tz = "CET")
+
+# Remove original df to save memory
 rm(df)
