a569d5f4683c613fd37d7ba0ab4dfec021289863
yxlee245 <41932856+yxlee245@users.noreply.github.com>
2020-02-19 21:41:40 +0800

    Added code to read in and subset data

---
+++ read_data.R
@@ -0,0 +1,4 @@
+# Read in data and get subset of data for 2007-02-01 and 2007-02-02
+df <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";",
+                 na.strings = "?", stringsAsFactors = FALSE)
+df_subset <- subset(df, Date == "1/2/2007" | Date == "2/2/2007")
