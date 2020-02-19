f5a5d0f0bf5467e078606f561ba656d1e7a35af5
yxlee245 <41932856+yxlee245@users.noreply.github.com>
2020-02-19 21:42:04 +0800

    Created codes for plot 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+# Read in data
+source("read_data.R")
+
+# Plot histogram
+hist(
+  df_subset$Global_active_power,
+  main = "Global Active Power",
+  xlab = "Global Active Power (kilowatts)",
+  col = "red"
+  )
+
+dev.copy(png,filename = "plot1.png", width = 480, height = 480, units = "px")
+dev.off(which = dev.cur())
